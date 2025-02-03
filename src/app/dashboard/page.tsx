9af35f986b3466ba3cc64b66b38ab84f4888a7d4
amalhanaja <amalhanaja@gmail.com>
2025-02-03 16:46:23 +0700

    fix: force dynamic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { LogoList } from "@/components/logo-list";
 import { Loader2 } from "lucide-react";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic"
+
 export default function DashboardPage() {
   return (
     <main className="w-full max-w-7xl mx-auto px-4 py-8 flex flex-col place-items-center">
